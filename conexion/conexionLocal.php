b6e644bab70ffc6a45d1bf59a52821117e133d71
kike <emailDePrueba@.com>
2021-04-19 12:42:43 +0200

    La base de datos se ha conectado exitosamente al servidor, ahora vamos ha crear otro archivo conexxion para local el cual se llama conexionLocal

---
+++ conexion/conexionLocal.php
@@ -0,0 +1,27 @@
+<?php
+
+class conexion{
+
+    private static $host = "eu-cdbr-west-01.cleardb.com";
+    private static $user = "b8fb8998d6289a";
+    private static $pass = "5db52478";
+    private static $database ="heroku_3c62ffd81ce121e";
+    private static $conexion;
+
+    public static function abrir_conexion(){
+
+        try{
+
+            self::$conexion = new mysqli(self::$host,self::$user,self::$pass,self::$database);
+
+        } catch(exception $e){
+
+            die("Error: " . $e->getMessage());
+
+        }
+        return self::$conexion;
+    }
+
+}
+
+?>
