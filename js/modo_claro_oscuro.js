6f4031b68fa0db75ea9de6449870929aed03aa4c
kike <emailDePrueba@.com>
2021-04-25 17:01:29 +0200

    Se a arreglado un bug donde el modo_claro_oscuro.js no guadaba correctamente en el local storage el valor

--- js/modo_claro_oscuro.js
+++ js/modo_claro_oscuro.js
@@ -3,7 +3,41 @@ $(document).ready(function () {
     cargar();
 
     $("#cambiar_modo").click(function () {
-        cambiar_modo();
+        document.querySelector(`#cambiar_modo i`).classList.toggle('fa-moon');
+        document.querySelector(`#cambiar_modo i`).classList.toggle('fa-sun');
+        document.getElementById('contenedor_central').classList.toggle('modoOscuro');
+        document.getElementById('cambiar_modo').classList.toggle('blanco');
+        document.getElementById('menu_categorias').classList.toggle('blanco');
+
+        guardar(document.getElementById("contenedor_central").classList.contains('modoOscuro'));
+
+        var productos = document.getElementsByClassName('contenedor_producto');
+
+        for (let i = 0; i < productos.length; i++) {
+
+            productos[i].classList.toggle('gris');
+        }
+
+        var descripcion = document.getElementsByClassName('descripcion_producto');
+
+        for (let i = 0; i < descripcion.length; i++) {
+
+            descripcion[i].classList.toggle('blanco');
+        }
+
+        var img_producto = document.getElementsByClassName('img_producto');
+
+        for (let i = 0; i < img_producto.length; i++) {
+
+            img_producto[i].classList.toggle('bBlanco');
+        }
+
+        var btn_comprar = document.getElementsByClassName('btn_comprar');
+
+        for (let i = 0; i < btn_comprar.length; i++) {
+
+            btn_comprar[i].classList.toggle('blanco');
+        }
 
     });
 
@@ -13,6 +47,8 @@ $(document).ready(function () {
         document.getElementById('contenedor_central').classList.toggle('modoOscuro');
         document.getElementById('cambiar_modo').classList.toggle('blanco');
         document.getElementById('menu_categorias').classList.toggle('blanco');
+
+        guardar(document.getElementById("contenedor_central").classList.contains('modoOscuro'));
 
         var productos = document.getElementsByClassName('contenedor_producto');
 
